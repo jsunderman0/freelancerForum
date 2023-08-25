82f2bde191f67297853a6986c3b3372f5a9adb95
Jack Sunderman <jacksunderman@Jacks-MacBook-Air.local>
2023-08-25 09:52:48 -0400

    first commit

--- freelancerForum.js
+++ freelancerForum.js
@@ -29,46 +29,75 @@ const names = [
     { name: "Dr. Wire", price: 47, occupation: "teacher" },
     { name: "Prof. Goose", price: 72, occupation: "driver" },
   ];
+//   function randomFreelancer(){
+//     const nameIdx = Math.floor(Math.random() * names.length);
+//     const occupationIdx = Math.floor(Math.random() * occupations.length);
+//     const priceIdx = (Math.ceil(Math.random()*100))
+//     const freelancer = {
+//         name: names[nameIdx],
+//         occupation: occupation[occupationIdx],
+//         price: priceIdx
+//     }
+//     return freelancer
+//   }
 
-  function randomName(){
-    const nameIdx = Math.floor(Math.random() * names.length)
-    const name = {
-        name: names[nameIdx]
-    }
-    return name
-  };
+//   const newfreelancers = [
+//     randomFreelancer()
+//   ]
 
-  randomName();
-
-  function randomOccupation(){
-    const occupationIdx = Math.floor(Math.random() * occupations.length)
-    const occupation = {
-        occupation: occupations[occupationIdx]
-    }
-    return occupation
-  };
-
-  function randomPrice(){
-    return (Math.ceil(Math.random()*100))
+//   function render(){
+//     const freelancerList = newfreelancers.map((freelancer) => {
+//         return `<li class = 
+//     })
+//   }
+//   function randomName(){
     
-  };
+//     const name = {
+//         name: names[nameIdx]
+//     }
+//     return name
+//   };
 
- function renderName(){
-    const nameList = names.map((name)=> {
-        return `<li class = "name"> ${name} </li>`
-    });
-    div.innerHTML = nameList.join('')
- };
+//   randomName();
 
- renderName()
+//   function randomOccupation(){
+    
+//     const occupation = {
+//         occupation: occupations[occupationIdx]
+//     }
+//     return occupation
+//   };
 
- function renderOccupation(){
-    const occupationList = occupations.map((occupation) => {
-        return `<li class = "occupation"> ${occupation} </li>`
-    });
-    div.innerHTML = occupationList.join('')
- };
+//   function randomPrice(){
+    
+    
+//   };
 
- renderOccupation();
+//  function renderName(){
+//     const nameList = names.map((name)=> {
+//         return `<li class = "name"> ${name} </li>`
+//     });
+//     div.innerHTML = nameList.join('')
+//  };
+
+//  renderName()
+
+//  function renderOccupation(){
+//     const occupationList = occupations.map((occupation) => {
+//         return `<li class = "occupation"> ${occupation} </li>`
+//     });
+//     div.innerHTML = occupationList.join('')
+//  };
+
+//  renderPrice();
+
+//  function renderOccupation(){
+//     const priceList = occupations.map((occupation) => {
+//         return `<li class = "occupation"> ${occupation} </li>`
+//     });
+//     div.innerHTML = occupationList.join('')
+//  };
+
+//  renderOccupation();
   
 
